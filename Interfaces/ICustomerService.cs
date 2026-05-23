528fa4872573cf4125bfb791b6d1a4e822138585
rohit1verma <vermarohit7780@gmail.com>
2026-05-23 23:07:50 +0530

    Controllers and interfaces

--- Interfaces/ICustomerService.cs
+++ Interfaces/ICustomerService.cs
@@ -1,6 +1,11 @@
-﻿namespace HotelBookingAPI.Interfaces
+﻿using HotelBookingAPI.DTOs;
+
+namespace HotelBookingAPI.Interfaces;
+
+public interface ICustomerService
 {
-    public interface ICustomerService
-    {
-    }
+    Task<AuthResponseDto> RegisterAsync(RegisterCustomerDto dto);
+    Task<AuthResponseDto> LoginAsync(LoginDto dto);
+    Task<CustomerResponseDto?> GetCustomerByIdAsync(int id);
+    Task<List<CustomerResponseDto>> GetAllCustomersAsync();
 }
